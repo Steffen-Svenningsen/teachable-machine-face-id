13a99a029bd87d42e36e69b093f1ac5b204cdc24
Steffen Svenningsen <stef7625@gmail.com>
2024-03-06 14:11:23 +0100

    Update ui

--- src/components/WebCam.vue
+++ src/components/WebCam.vue
@@ -4,54 +4,58 @@ import { ref } from "vue";
 
 const videoActive = ref(false);
 
- const URL = "https://teachablemachine.withgoogle.com/models/WUjKsLUR-/";
+const URL = "https://teachablemachine.withgoogle.com/models/WUjKsLUR-/";
 
-    let model, webcam, labelContainer, maxPredictions;
+let model, webcam, labelContainer, maxPredictions;
 
-    // Load the image model and setup the webcam
-    async function init() {
-        const modelURL = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
 
-        // load the model and metadata
-        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-        // or files from your local hard drive
-        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-        model = await tmImage.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
 
-        // Convenience function to setup a webcam
-        const flip = true; // whether to flip the webcam
-        webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-        await webcam.setup(); // request access to the webcam
-        await webcam.play();
-        videoActive.value = true;
-        window.requestAnimationFrame(loop);
+    // Convenience function to setup a webcam
+    const flip = true; // whether to flip the webcam
+    webcam = new tmImage.Webcam(300, 300, flip); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    videoActive.value = true;
+    window.requestAnimationFrame(loop);
 
-        // append elements to the DOM
-        document.getElementById("webcam-container").appendChild(webcam.canvas);
-        labelContainer = document.getElementById("label-container");
-        for (let i = 0; i < maxPredictions; i++) { // and class labels
-            labelContainer.appendChild(document.createElement("div"));
-        }
+    // append elements to the DOM
+    const webcamCanvas = document.getElementById("webcam-container").appendChild(webcam.canvas);
+    webcamCanvas.style.borderRadius = "6px";
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        const labelChild = labelContainer.appendChild(document.createElement("div"));
+        labelChild.style.borderRadius = "6px";
+        labelChild.style.border = "1px solid #acacac";
+        labelChild.style.padding = "1rem";
     }
+}
 
-    async function loop() {
-        webcam.update(); // update the webcam frame
-        await predict();
-        window.requestAnimationFrame(loop);
-    }
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
 
-    // run the webcam image through the image model
-    async function predict() {
-        // predict can take in an image, video or canvas html element
-        const prediction = await model.predict(webcam.canvas);
-        for (let i = 0; i < maxPredictions; i++) {
-            const classPrediction =
-                prediction[i].className + ": " + (prediction[i].probability * 100).toFixed(0) + "%";
-            labelContainer.childNodes[i].innerHTML = classPrediction;
-        }
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ": " + (prediction[i].probability * 100).toFixed(0) + "%";
+        labelContainer.childNodes[i].innerHTML = classPrediction;
     }
+}
 
 const stopRecording = () => {
     webcam.stop();
@@ -65,13 +69,39 @@ const stopRecording = () => {
 
 <template>
     <div id='webcam-container'></div>
-    <button v-if="videoActive" type="button" @click="stopRecording">Stop</button>
-    <button v-else type='button' @click="init">Start</button>
     <div id='label-container'></div>
+    <button class="stop-btn" v-if="videoActive" type="button" @click="stopRecording">Stop</button>
+    <button class="start-btn" v-else type='button' @click="init">Start</button>
 </template>
 
 <style lang="sass" scoped>
 button
     padding: 0.5rem 1rem
     cursor: pointer
+    position: relative
+    margin: 1rem
+
+    &::after
+        position: absolute
+        font-size: 20px
+        width: 100%
+        height: 100%
+        top: 0
+        left: 0
+        transform: translate(50%, -50%)
+
+.start-btn
+    &::after
+        content: '📸'
+        transform: translate(50%, -50%) rotate(-20deg)
+
+.stop-btn
+    &::after
+        content: '❌'
+
+#label-container
+    width: 300px
+    display: flex
+    flex-direction: column
+    gap: 8px
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,25 @@ import WebCam from '@/components/WebCam.vue';
 
 <template>
   <main>
-    <h1>Face Recognition</h1>
+    <h1>Person Recognition</h1>
+    <p>By pressing start You'll be able to turn on your webcam/camera to detect weither there's a person in the picture through <a target="_blank" href="https://teachablemachine.withgoogle.com/">Machine Learning Models</a>.</p>
     <WebCam />
   </main>
 </template>
+
+<style lang="sass" scoped>
+h1, p
+  max-width: 420px
+  text-align: center
+  line-height: 1.2
+  margin: 1rem 0 0.5rem 0
+
+  a
+    color: #487E01
+    text-decoration: none
+    filter: saturate(150%)
+    transition: 0.2s
+
+    &:hover
+      filter: saturate(400%)
+</style>
